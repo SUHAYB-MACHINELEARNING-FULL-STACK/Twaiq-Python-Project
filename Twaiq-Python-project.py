ddd53ee11b5b57ebb0a2aedfb4315b7b4391493d
White_Hat_Hacker <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-08-27 09:37:55 +0300

    Update Twaiq-Python-project.py

--- Twaiq-Python-project.py
+++ Twaiq-Python-project.py
@@ -1,5 +1,5 @@
 info = {
-  "1212121212":"mohammed"
+  "1212121212":"mohammed" # For Example
 }
 Input = input('Your Name Or Phone Number: ').lower()
 print("Sorry That's Error" if Input not in info.keys() and Input not in info.values() else [i for i in info.keys() if info[i]==Input][0] if Input in info.values() else info[Input])

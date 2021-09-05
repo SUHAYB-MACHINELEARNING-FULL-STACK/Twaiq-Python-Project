0b3d97243fa9d5b111cc32cc90845a178343f93e
White_Hat_Hacker <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-09-05 19:07:09 +0300

    Update Twaiq-Python-project.py

--- Twaiq-Python-project.py
+++ Twaiq-Python-project.py
@@ -2,4 +2,4 @@ info = {
   "1212121212":"mohammed" # For Example
 }
 Input = input('Your Name Or Phone Number: ').lower()
-print("Sorry That's Error" if Input not in info.keys() and Input not in info.values() else [i for i in info.keys() if info[i]==Input][0] if Input in info.values() else info[Input])
+print("Sorry, The number is not found" if Input not in info.keys() and Input not in info.values() else "This is invalid number" if len([i for i in Input if i in "0123456789"])!=len(Input) else [i for i in info.keys() if info[i]==Input][0] if Input in info.values() else info[Input])
